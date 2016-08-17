30c054c9414b533f4acdd059407e5d62e42bb1bb
Manit Kwanyun <a@muime.com>
2016-08-17 17:01:55 +0700

    แก้ไขให้ route กับ controller อยู่คนละไฟล์กัน

--- routes/route.js
+++ routes/route.js
@@ -1,21 +1,34 @@
 
-var app=angular.module('MainApp',[]);
-var app=angular.module('MainApp',['ngRoute']);
+//var app=angular.module('MainApp',[]);
+//var app=angular.module('MainApp',['ngRoute']);
+//
+//app.config(['$routeProvider',
+//  function($routeProvider) {
+//    $routeProvider.
+//      when('/project', {
+//		templateUrl: 'testangular.html',
+//		//controller: 'myCtrl'
+//	});
+//
+//}]);
 
-app.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/project', {
-		templateUrl: 'testangular.html',
-		//controller: 'myCtrl'
-	});
+//app.controller('myCtrl', function ($scope) {
+//    $scope.firstName = "John";
+//    $scope.lastName = "Doe";
+//});
 
+var app = angular.module('MainApp', ['ui.router']);
+
+// configure our routes
+app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function ($stateProvider, $urlRouterProvider, $locationProvider) {
+    //$locationProvider.html5Mode(true);
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+        .state('root', {
+            url: '/',
+            templateUrl: 'testangular.html',
+            controller: 'myCtrl'
+        });
 }]);
-
-
-app.controller('myCtrlA', function($scope) {
-
-	 $scope.firstName = "John";
-    $scope.lastName = "Doe";
-
-});
